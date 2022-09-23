90c639e0cbd4159341443cfdd3b9dd8e488c7e2e
Giovedi Marmolejo Cruz <gmarmolejo@novatronic.com>
2022-09-23 16:07:30 -0500

    Frist Commit

---
+++ myapp/models.py
@@ -0,0 +1,12 @@
+from tkinter import CASCADE
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)    
