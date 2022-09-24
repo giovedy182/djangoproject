bec38c938d3413d5b138bedcd115604b63f6303d
Giovedy Marmolejo WIN <giovedy.182@gmail.com>
2022-09-23 23:14:59 -0500

    third version

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
-    path('about/', views.about)
+    path('', views.index),
+    path('about/', views.about),
+    #path('hello/<int:id>', views.hello),
+    path('hello/<str:username>', views.hello),
+    path('project/', views.project),
+    path('task/<int:id>', views.task)
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+def index(request):
+    return HttpResponse("Index Page")
+
+def hello(request, username):
+    return HttpResponse("<h2>Hello %s</h2>" % username)
+
+#def hello(request, id):
+    #print(type(id))
+    #result=id + 100 * 2
+    #return HttpResponse("<h2>Hello %s</h2>" % result)
 
 def about(request):
     return HttpResponse('about')
+
+def project(request):
+    project = list(Project.objects.values())
+    return JsonResponse(project, safe=False)
+
+def task(request, id):
+    # task = Task.objects.get(id=id)
+    task = get_object_or_404 (Task, id=id)
+    return HttpResponse('task %s' % task.title)
