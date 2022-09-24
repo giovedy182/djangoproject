b5c284f39a9390ab6ac0b6dd5cb16c4868cb259d
Giovedy Marmolejo WIN <giovedy.182@gmail.com>
2022-09-23 21:50:42 -0500

    Second Release

--- myapp/models.py
+++ myapp/models.py
@@ -9,4 +9,4 @@ class Project(models.Model):
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)    
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iebvc163q8^w%ott&hgv4r1@u@!4m%-s+nj_*0%aokj)bahy38
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
